b6ec3aca16896e2a8f69d10acbd9b4e77f6e2d6d
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2025-06-26 22:11:57 +0530

    added solution boilerplate

---
+++ solutions/tdd-file-loader/src/main/java/com/example/tutorial/tdd-file-loader/App.java
@@ -0,0 +1,10 @@
+package com.example.tutorial.tdd-file-loader;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
