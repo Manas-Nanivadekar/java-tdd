9513487857a6688c872b7e123a906d07f008e8f3
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2025-06-28 10:58:30 +0530

    added solutions for findHigestNumberTest

--- solutions/find-highest-number/src/test/java/com/example/findhighestnumber/HighestNumberFinderTest.java
+++ solutions/find-highest-number/src/test/java/com/example/findhighestnumber/HighestNumberFinderTest.java
@@ -1,5 +1,87 @@
 package com.example.findhighestnumber;
 
-public class HighestNumberFinderTest {
-    // Your code here
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class HighestNumberFinderTest
+{
+    @Test
+    public void find_highest_in_array_of_one_expect_single_item() 
+    {
+        // Arrange
+        int array[] = 
+        {
+            10
+        };
+        HighestNumberFinder cut = new HighestNumberFinder();
+        int expectedResult = 10;
+        
+        // Act
+        int result = cut.findHighestNumber( array );
+        
+        // Assert
+        assertEquals( expectedResult, result );
+    }
+    
+    @Test
+    public  void    find_highest_in_array_of_two_descending_expect_first_element()
+    {
+        // Arrange
+        int array[] = {20, 10};
+        int expectedResult = 20;
+        HighestNumberFinder cut = new HighestNumberFinder();
+        
+        // Act
+        int result = cut.findHighestNumber(array);
+        
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+    
+    @Test
+    public  void    find_highest_in_array_of_two_ascending_expect_second_element()
+    {
+        // Arrange
+        int array[] = {10, 20};
+        int expectedResult = 20;
+        HighestNumberFinder cut = new HighestNumberFinder();
+        
+        // Act
+        int result = cut.findHighestNumber(array);
+        
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+    
+    @Test
+    public  void    find_highest_in_array_of_two_equals_expect_first_element()
+    {
+        // Arrange
+        int array[] = {10, 10};
+        int expectedResult = 10;
+        HighestNumberFinder cut = new HighestNumberFinder();
+        
+        // Act
+        int result = cut.findHighestNumber(array);
+        
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+    
+    @Test
+    public  void    find_highest_in_array_of_several_items_expect_highest_element()
+    {
+        // Arrange
+        int array[] = {10, 20, 5, 17, 37, 14};
+        int expectedResult = 37;
+        HighestNumberFinder cut = new HighestNumberFinder();
+        
+        // Act
+        int result = cut.findHighestNumber(array);
+        
+        // Assert
+        assertEquals(expectedResult, result);
+    }
 }
